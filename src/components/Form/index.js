118ac9cbf2c9d1d4d40e53ea60c8b432f601afeb
oguuzduran <oguzduran@outlook.com.tr>
2022-04-04 09:41:45 +0300

    header edited

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,10 +5,10 @@ const Form = () => {
 		<>
 			<div className="form">
 				<form>
-					<input type="text" value="no giriniz." name="id" />
-					<input type="text" value="kontrat giriniz." name="kontrat" />
-					<input type="text" value="teklif giriniz." name="teklif" />
-					<input type="text" value="data giriniz." name="data" />
+					<input type="text" defaultValue="no giriniz." name="id" />
+					<input type="text" defaultValue="kontrat giriniz." name="kontrat" />
+					<input type="text" defaultValue="teklif giriniz." name="teklif" />
+					<input type="text" defaultValue="data giriniz." name="data" />
 					<button>Kaydet</button>
 				</form>
 				<button>Yeni Ekle</button>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './Header.scss';
 
+import epias from '../../images/svg/epias.svg';
+import layout from '../../images/svg/layout.svg';
+
 function Header() {
 	return (
 		<div className="Header">
-			<div>Epiaş</div>
+			<div>
+				<img src={epias} width="70px" alt="" />
+			</div>
 			<div className="ClientInfo">
-				<div className="WorkingArea">Çalışma Alanı</div>
-				<div className="ClientSettings">Merhab, Oğuz</div>
+				<div className="WorkingArea">
+					<img
+						src={layout}
+						alt=""
+						width="30px"
+						// style={{ backgroundColor: '#fff', marginRight: '10px' }}
+						className="layout-svg"
+					/>
+					<span>Çalışma Alanı</span>
+				</div>
+				<div className="ClientSettings">
+					<span data-letters="MN" />
+					Merhab, Oğuz
+				</div>
 			</div>
 		</div>
 	);
